95f0f1d1b43a80b7b19ebc13c98ff28c64c6a023
Jean-Marc Strauven <jms@grazulex.be>
2025-06-15 03:17:37 +0200

    chore: fix phpstan configuration and remove old test files

--- src/Traits/DTOFromModelTrait.php
+++ src/Traits/DTOFromModelTrait.php
@@ -7,24 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Trait to create DTOs from Eloquent models.
- * 
+ *
  * This trait provides convenient methods to create DTO instances from
  * Eloquent models with support for relations and collections.
- * 
+ *
  * @example
  * ```php
  * class UserDTO extends LaravelArcDTO
  * {
  *     use FromModelTrait;
- *     
+ *
  *     #[Property(type: 'string', required: true)]
  *     public string $name;
  * }
- * 
+ *
  * // Create DTO from model
  * $user = User::with('profile')->find(1);
  * $userDTO = UserDTO::fromModel($user, ['profile']);
- * 
+ *
  * // Create DTOs from collection
  * $users = User::with('profile')->get();
  * $userDTOs = UserDTO::fromModels($users, ['profile']);
@@ -34,20 +34,21 @@ trait DTOFromModelTrait
 {
     /**
      * Create DTO instance from Eloquent model.
-     * 
+     *
      * @param Model $model The Eloquent model to convert
      * @param array<string> $relations Relations to include (must be loaded)
+     *
      * @return static New DTO instance
      */
     public static function fromModel(Model $model, array $relations = []): static
     {
         $data = $model->toArray();
-        
+
         // Include loaded relations if specified
         foreach ($relations as $relation) {
             if ($model->relationLoaded($relation)) {
                 $relationData = $model->{$relation};
-                
+
                 if ($relationData instanceof Collection) {
                     // Handle collection relations (HasMany, BelongsToMany)
                     $data[$relation] = $relationData->toArray();
@@ -60,42 +61,44 @@ trait DTOFromModelTrait
                 }
             }
         }
-        
+
         return new static($data);
     }
-    
+
     /**
      * Create multiple DTO instances from collection of models.
-     * 
+     *
      * @param Collection<int, Model> $models Collection of Eloquent models
      * @param array<string> $relations Relations to include (must be loaded)
+     *
      * @return array<static> Array of DTO instances
      */
     public static function fromModels(Collection $models, array $relations = []): array
     {
-        return $models->map(fn(Model $model) => static::fromModel($model, $relations))->toArray();
+        return $models->map(fn (Model $model) => static::fromModel($model, $relations))->toArray();
     }
-    
+
     /**
      * Create DTO instance from model with automatic relation detection.
-     * 
+     *
      * This method automatically includes all loaded relations without
      * needing to specify them explicitly.
-     * 
+     *
      * @param Model $model The Eloquent model to convert
+     *
      * @return static New DTO instance
      */
     public static function fromModelWithLoadedRelations(Model $model): static
     {
         $data = $model->toArray();
-        
+
         // Get all loaded relations
         $loadedRelations = array_keys($model->getRelations());
-        
+
         // Include all loaded relations
         foreach ($loadedRelations as $relation) {
             $relationData = $model->{$relation};
-            
+
             if ($relationData instanceof Collection) {
                 $data[$relation] = $relationData->toArray();
             } elseif ($relationData instanceof Model) {
@@ -104,19 +107,19 @@ trait DTOFromModelTrait
                 $data[$relation] = $relationData;
             }
         }
-        
+
         return new static($data);
     }
-    
+
     /**
      * Create multiple DTO instances with automatic relation detection.
-     * 
+     *
      * @param Collection<int, Model> $models Collection of Eloquent models
+     *
      * @return array<static> Array of DTO instances
      */
     public static function fromModelsWithLoadedRelations(Collection $models): array
     {
-        return $models->map(fn(Model $model) => static::fromModelWithLoadedRelations($model))->toArray();
+        return $models->map(fn (Model $model) => static::fromModelWithLoadedRelations($model))->toArray();
     }
 }
-
